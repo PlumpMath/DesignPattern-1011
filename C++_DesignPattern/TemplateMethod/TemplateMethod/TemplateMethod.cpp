3dd5b67212cf8c09f4b266b12a9fb404f0cea6d8
XiongZiqian <ziqian0716@163.com>
2016-09-13 11:35:56 +0800

    add some new patterns

--- C++_DesignPattern/TemplateMethod/TemplateMethod/TemplateMethod.cpp
+++ C++_DesignPattern/TemplateMethod/TemplateMethod/TemplateMethod.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
+#include "TestPaper.h"
 
+#include <iostream>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout<<"学生甲抄的试卷";
+	TestPaper* studentA = new TestPaperA();
+	studentA->TestQuestion1();
+	studentA->TestQuestion2();
+	studentA->TestQuestion3();
+
+	cout<<"学生乙抄的试卷";
+	TestPaper* studentB = new TestPaperB();
+	studentB->TestQuestion1();
+	studentB->TestQuestion2();
+	studentB->TestQuestion3();
 	return 0;
 }
 
